fcd920fea86a1fa3c8d47a3504e7e87ca73ef1a5
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-05 22:17:57 -0500

    Practiced the problem a needle in the haystack

--- main.js
+++ main.js
@@ -82,3 +82,28 @@ function boolToWord(bool) {
     return "No";
   }
 }
+
+//faster and easier from previous problem
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+//Can you find the needle in the haystack?
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+//"found the needle at position " plus the index it found the needle, so:
+//["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] == "needle") {
+      return `found the needle at position ${i}`;
+    }
+  }
+}
+findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]);
+
+// easier and fater way for the top one
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
