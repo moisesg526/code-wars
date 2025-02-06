59d07c870be067e758a796e5f3115b9380261846
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-05 21:52:32 -0500

    Practiced the problem convert boolean values to string yes or no

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//         Use the PREP strategy to complete each problem
+// P = Parameters
+// R = Returns
+// E = Example
+// P = Pseudocode
+
 // Write a function "greet" that returns "hello world!"
 
 function greet() {
@@ -55,3 +61,24 @@ function stringToArray(string) {
 }
 
 stringToArray("Robin Singh");
+
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers) {
+  let sum = 0;
+  numbers.forEach(x, (i) => {
+    sum += Math.pow(x, 2);
+  });
+  return sum;
+}
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+  if (bool == true) {
+    return "Yes";
+  } else if (bool == false) {
+    return "No";
+  }
+}
