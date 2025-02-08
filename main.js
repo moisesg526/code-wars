1c005a0f5781e673772b9dc910dbaac39f7f8ad4
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-07 22:01:21 -0500

    Gave a link to the hvac website page in recent work

--- main.js
+++ main.js
@@ -127,30 +127,29 @@ function rentalCarCost(d) {
 }
 
 function checkNumber(arr) {
-
-    if(arr[0] > arr[arr.length - 1]) {
-      alert("hi")
-    } else if(arr[0] < arr[arr.length - 1]) {
-      alert("bye")
-    }  else {
-      alert("we close in an hour")
-    }
+  if (arr[0] > arr[arr.length - 1]) {
+    alert("hi");
+  } else if (arr[0] < arr[arr.length - 1]) {
+    alert("bye");
+  } else {
+    alert("we close in an hour");
   }
-checkNumber([2,3,4,56,-2])
+}
+checkNumber([2, 3, 4, 56, -2]);
 
+//You get an array of numbers, return the sum of all of the positives ones.
+//Example [1, -4, 7, 12] => 1 + 7 + 12 = 20
+// If there is nothing to sum, the sum is default to 0.
 
-You get an array of numbers, return the sum of all of the positives ones.
+function positiveSum(arr) {
+  let sum = 0;
+  arr.forEach((num) => {
+    if (num > 0) {
+      sum += num;
+    } else {
+      return 0;
+    }
+  });
+}
 
-Example
-[1, -4, 7, 12] => 
-1
-+
-7
-+
-12
-=
-20
-1+7+12=20
-Note
-If there is nothing to sum, the sum is default to 0.
-function 
+positiveSum([1, 2, -3, -4, 3, 5]);
