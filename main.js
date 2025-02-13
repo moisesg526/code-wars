327a43087e0d499ac27d2571a6b3b15e1a61adfa
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-12 23:24:36 -0500

    Practiced the problem sum of odd numbers in nth row of a triangle

--- main.js
+++ main.js
@@ -189,13 +189,27 @@ stringToNumber("1233432");
 
 function countBy(x, n) {
   let z = [];
-  let multiplesOfX = 1;
-  for (let i = 1; i <= n; i++) {
-    multiplesOfX = x * i;
-    z.push(multiplesOfX);
-    multiplesOfX = 1;
+  for (let i = 1; i < n.length; i++) {
+    z.push(x * i);
   }
-  console.log(z);
+  return z;
 }
 
 countBy(5, 5);
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1)
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumber(n) {
+  return Math.pow(n, 3);
+}
+rowSumOddNumber(4);
