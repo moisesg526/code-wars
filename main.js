2c72511a0fa14d645fea144c2926a623400318bb
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-13 23:10:44 -0500

    Practiced dna strand replacing A to T, C to G, T to A, and G to C

--- main.js
+++ main.js
@@ -223,5 +223,28 @@ rowSumOddNumber(4);
 
 function filter_list(l) {
   // Return a new array with the strings filtered out
-  return l.filter((str) => typeof str != "string" && str >= 0);
+  return l.filter((num) => typeof num === "number");
 }
+console.log(filter_list([1, 2, "a", "b", 4, 6, 7, "r"]));
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+// Your function receives one side of the DNA (string, except for Haskell); you need to return the
+// other complementary side. DNA strand is never empty or there is no DNA at all (again, except for
+// Haskell).
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function dnaStrand(dna) {
+  const DNAComplement = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "T",
+  };
+
+  return dna
+    .split("")
+    .map((elem) => DNAComplement[elem])
+    .join("");
+}
+dnaStrand("GGCTTGAA")
