bf838bf99ada1bb94dc6e0826fd8fc77dfb6e740
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-14 22:03:57 -0500

    Practiced the problem sum of two positive numbers

--- main.js
+++ main.js
@@ -247,4 +247,18 @@ function dnaStrand(dna) {
     .map((elem) => DNAComplement[elem])
     .join("");
 }
-dnaStrand("GGCTTGAA")
+dnaStrand("GGCTTGAA");
+
+// Sum of two lowest positive integers
+// Create a function that returns the sum of the two lowest positive numbers given an
+// array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+function sumTwoSmallestNumbers(numbers) {
+  let sum;
+  let newArr = numbers.sort((a, b) => a - b);
+  sum = newArr[0] + newArr[1];
+  return sum;
+}
+
+sumTwoSmallestNumbers([2, 3, 4, 9, 5, 1]);
