b94a6658539515a46f099d520ab40ad03dadd5b5
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-15 23:58:53 -0500

    Practiced the problem convert a number to string

--- main.js
+++ main.js
@@ -262,3 +262,14 @@ function sumTwoSmallestNumbers(numbers) {
 }
 
 sumTwoSmallestNumbers([2, 3, 4, 9, 5, 1]);
+
+
+// We need a function that can transform a number (integer) into a string.
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString()
+}
