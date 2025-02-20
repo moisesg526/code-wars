ded3781de09040d0c8c0203f2ae44e3764a12d3e
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-19 22:17:48 -0500

    Practiced the problem reverse words

--- main.js
+++ main.js
@@ -299,3 +299,17 @@ function friend(friends) {
 }
 
 friend(["Michael", "Scot", "Jim", "John", "Pete", "Ryan"]);
+
+// Reverse Word
+// Complete the function that accepts a string parameter, and reverses each word in the string. 
+// All spaces in the string should be retained.
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  // Go for it
+ return str.split("").reverse().join("").split(" ").reverse().join(" ")
+ 
+}
+
+reverseWords("Hello everyone lets code!")
