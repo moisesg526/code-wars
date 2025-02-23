d0169abacff8312e687aa4d1b7b5b776720211a8
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-22 23:57:52 -0500

    Practiced problem descending order

--- main.js
+++ main.js
@@ -371,3 +371,18 @@ function areaOrPerimeter(l, w) {
 }
 
 areaOrPerimeter(3, 8);
+
+//Descending Order
+//Your task is to make a function that can take any non-negative integer as an argument
+// and return it with its digits in descending order. Essentially, rearrange the digits to
+// create the highest possible number.
+//Ex. Input: 42145 Output: 54421
+
+function descendingOrder(n) {
+  let sortedArr = n.sort((a, b) => {
+    b - a;
+  });
+  return sortedArr;
+}
+
+descendingOrder([2, 1, 3, 4, 3, 6]);
