9ebbcb5dd9286fbcdaab83184259837f377cda72
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-02-24 23:57:20 -0500

    Practiced disemvowel trolls

--- main.js
+++ main.js
@@ -416,3 +416,34 @@ function removeEveryOther(arr) {
 }
 
 removeEveryOther(["Hello", "Goodbye", "Hello", "Goodbye"]);
+
+// Disemvowel Trolls
+//Trolls are attacking your comment section!
+
+//A common way to deal with this situation is to remove all of the vowels from the trolls'
+// comments, neutralizing the threat.
+
+//Your task is to write a function that takes a string and return a new string with all v
+// owels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+  let newString = [];
+  str.split("");
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] !== "a" ||
+      str[i] !== "e" ||
+      str[i] !== "i" ||
+      str[i] !== "o" ||
+      str[i] !== "u"
+    ) {
+      newString.push(str[i]);
+    }
+  }
+  newString.join("").toString();
+  return newString;
+}
