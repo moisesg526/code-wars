5d5ec012dee67cdd861e116064338dc5d3058d00
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-03-03 23:18:37 -0500

    Returned the total goals in all three leagues

---
+++ messiGoals.js
@@ -0,0 +1,14 @@
+// Messi is a soccer player with goals in three leagues:
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+// Note: the input will always be valid.
+// ex. 5, 10, 2  -->  17
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+goals(30, 43, 32);
