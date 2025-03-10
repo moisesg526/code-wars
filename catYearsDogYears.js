a678b94056f967ec14a04a2232e3faeb89101fee
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-03-09 23:07:44 -0400

    Created a function that checked if a walk would return true if the walk the app gave you took you 10 minutes and got you back to the starting location

--- catYearsDogYears.js
+++ catYearsDogYears.js
@@ -17,7 +17,7 @@ var humanYearsCatYearsDogYears = function (humanYears) {
   if (humanYears === 1) {
     catYears += 15;
     dogYears += 15;
-  } else if (humanYears < 3) {
+  } else if (humanYears === 2) {
     catYears += 24;
     dogYears += 24;
   } else if (humanYears > 2) {
