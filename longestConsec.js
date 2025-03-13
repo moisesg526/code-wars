59ac902ea8ba834b54bc0accd81cca7db87b1193
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-03-13 00:34:53 -0400

    Created a function that joined the longest consecutive k strings in the array

---
+++ longestConsec.js
@@ -0,0 +1,18 @@
+function longestConsec(strarr, k) {
+  if (strarr.length == 0 || k > strarr.length || k <= 0) return "";
+
+  let longStr = "";
+
+  let newStr = "";
+
+  for (let i = 0; i < strarr.length; i++) {
+    newStr = strarr.slice(i, i + k);
+    if (newStr.join("").length > longStr.length) {
+      longStr = newStr.join("");
+    }
+  }
+
+  return longStr;
+}
+
+longestConsec(["A", "AB", "ABC", "D", "DEF", "GHIJK"], 2);
