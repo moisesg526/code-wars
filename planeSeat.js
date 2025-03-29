79da9fd60ecf38c6f3ef4c144dc31ae8aac02974
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-03-28 23:21:02 -0400

    Practiced the problem Holiday II plane seating

---
+++ planeSeat.js
@@ -0,0 +1,15 @@
+function planeSeat(a) {
+  if (a.includes("B")) {
+    console.log("Left");
+  }
+}
+
+planeSeat("30B");
+
+// 1-20 = Front
+// 21-40 = Middle
+// 41-60 = Back
+
+// A, B, C = Left
+// D, E, F = Middle
+// G, H, K = Right
