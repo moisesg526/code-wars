31e6082ea563ca2cfeae13b05226e11cc660b39c
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-04-27 14:42:22 -0400

    Practiced problem grasshopper

---
+++ grasshopper.js
@@ -0,0 +1,18 @@
+function move(position, roll) {
+  // return the new position
+
+  return position + roll * 2;
+
+  //   let currentPosition;
+  //   if (position === 0) {
+  //     currentPosition = 0;
+  //     currentPosition += roll * 2;
+  //     console.log(currentPosition);
+  //   } else {
+  //     currentPosition = position;
+  //     currentPosition += roll * 2;
+  //     console.log(currentPosition);
+  //   }
+}
+
+move(2, 4);
